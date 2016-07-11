cc5497d91cd8b8bb893b5d6518776cc5b99db594
theSoenke <theSoenke@users.noreply.github.com>
2016-07-11 22:09:55 +0200

    Show protobox permission dialog

--- client/js/lounge.js
+++ client/js/lounge.js
@@ -1106,3 +1106,27 @@ $(function() {
 		}
 	);
 });
+
+// sandstorm network permission
+function doRequest (serializedPowerboxDescriptor) {
+  window.parent.postMessage({ powerboxRequest: {
+      rpcId: '1',
+      query: [serializedPowerboxDescriptor]
+  }}, '*');
+}
+
+interfaces = {
+  // Powerbox descriptors for various interface IDs.
+  // from https://github.com/zarvox/sandstorm-test-python/blob/master/templates/index.html
+  ipNetwork: 'EAZQAQEAABEBF1EEAQH_QCAqemtXgqkAAAA', // 12214421258504904768,
+  ipInterface: 'EAZQAQEAABEBF1EEAQH_-tY-6W5QLOMAAAA', // 16369547182874744570,
+}
+
+var messageListener = function (event) {
+	// TODO implement capnproto binding
+  console.log('powerbox network access granted');
+}
+
+window.addEventListener('message', messageListener);
+
+doRequest(interfaces.ipNetwork);
